3be451634da9f6a08b72b742d67fa011edc84d09
Lines3 <100088091+Lines3@users.noreply.github.com>
2023-10-08 21:21:09 +0200

    Create tickets.js

---
+++ js/tickets.js
@@ -0,0 +1,89 @@
+const purchaseForm = document.querySelector("#formPurchase");
+const firstName = document.querySelector("#firstName");
+const firstNameError = document.querySelector("#firstNameError");
+const lastName = document.querySelector("#lastName");
+const lastNameError = document.querySelector("#lastNameError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const tickets = document.querySelector("#tickets");
+const ticketsError = document.querySelector("#ticketsError");
+const cardPaymentForm = document.querySelector("#formPurchase");
+const cardNumber = document.querySelector("#cardNumber");
+const cardNumberError = document.querySelector("#cardNumberError");
+const nameOnCard = document.querySelector("#nameOnCard");
+const nameOnCardError = document.querySelector("#nameOnCardError");
+const expiryDate = document.querySelector("#expiryDate");
+const expiryDateError = document.querySelector("#expiryDateError");
+const securityCode = document.querySelector("#securityCode");
+const securityCodeError = document.querySelector("#securityCodeError");
+
+function validateForm() {
+    event.preventDefault();
+
+    if (firstName.value.trim().length > 0) {
+        firstNameError.style.display = "none";
+    }
+    else {
+        firstNameError.style.display = "block";
+    }
+
+    if (lastName.value.trim().length > 0) {
+        lastNameError.style.display = "none";
+    }
+    else {
+        lastNameError.style.display = "block";
+    }
+
+    if (validateEmail(email.value) === true) {
+        emailError.style.display = "none";
+    }
+    else {
+        emailError.style.display = "block";
+    }
+
+    if (tickets.value > 0) {
+        ticketsError.style.display = "none";
+    }
+    else {
+        ticketsError.style.display = "block";
+    }
+    if (cardNumber.value) {
+        cardNumberError.style.display = "none";
+    }
+    else {
+        cardNumberError.style.display = "block";
+    }
+
+    if (nameOnCard.value.trim().length > 0) {
+        nameOnCardError.style.display = "none";
+    }
+    else {
+        nameOnCardError.style.display = "block";
+    }
+
+    if (expiryDate.value) {
+        expiryDateError.style.display = "none";
+    }
+    else {
+        expiryDateError.style.display = "block";
+    }
+
+    if (securityCode.value) {
+        securityCodeError.style.display = "none";
+    }
+    else {
+        securityCodeError.style.display = "block";
+    }
+
+    // console.log();
+};
+
+
+purchaseForm.addEventListener("submit", validateForm);
+
+
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+}
